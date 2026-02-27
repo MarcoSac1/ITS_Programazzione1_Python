32d8a7132f542872d3afd6378e9a2bde48ee6e25
Marcosac1 <marcosaccone8@gmai.com>
2026-02-27 13:05:23 +0100

    dictionary testing

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+book = {
+        "titolo": "biancaneve e i 7 nani",
+        "autore": "francofarm",
+        "anno":     "2033",
+}
+
+book["pages"] =7384
+
+for key, value in book.items():
+    print(f"{key=}",f"{value=}")
+
